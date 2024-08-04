5208eac9c3ac01d0d12f6b5c0a1fd8610c1835a7
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2024-08-04 08:51:30 -0300

    Inicialiação do projeto utilizando TDD. Criação do projeto  de testes e a primeiro método com assinatura inicial para testes.

---
+++ Nivelamento_GTF_202408.CalculoCDB/CalculoCDB.Tests/CalculoCDB/Servico/CalculoCDB_TestesServico.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoCDB.Tests.CalculoCDB.Servico
+{
+    public class CalculoCDB_TestesServico
+    {
+        private readonly ICalculoCDBService _calculoCDBService;
+
+        public CalculoCDB_TestesServico()
+        {
+            _calculoCDBService = new CalculoCDBService();
+        }
+
+        [Fact(DisplayName = "Deve Efetuar um Calculo do CBD com sucesso")]
+        public void DeveEfetuarCalculoCdb_ComSucesso()
+        {
+            //Arrange
+
+            //Act
+            var valorCalculado = _calculoCDBService.CalcularCBD(1, 1200m);
+
+            //Assert
+            Assert.True(valorCalculado.Item1 > 0 && valorCalculado.Item2 > 0);
+        }
+    }
+
+    public class CalculoCDBService : ICalculoCDBService
+    {
+        /// <summary>
+        /// Efetua o Cáculo de um Investimento CBD
+        /// </summary>
+        /// <param name="prazo"></param>
+        /// <param name="valorInvestir"></param>
+        /// <returns>ValorFinalBruto, ValorFinalLiquido</returns>
+        public Tuple<decimal, decimal> CalcularCBD(int prazo, decimal valorInvestir)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ICalculoCDBService
+    {
+        /// <summary>
+        /// Efetua o Cáculo de um Investimento CBD
+        /// </summary>
+        /// <param name="prazo"></param>
+        /// <param name="valorInvestir"></param>
+        /// <returns>ValorFinalBruto, ValorFinalLiquido</returns>
+        Tuple<decimal, decimal> CalcularCBD(int prazo, decimal valorInvestir);
+    } 
+}
