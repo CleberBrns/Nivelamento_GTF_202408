dfd2fde7a81a9abbc16b82ef73df5e2117723f0f
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2024-08-05 10:18:24 -0300

    Implementação de Interface e classes que implementam essa interface para calcular diferentes tipos de Impostos. Criação de Classe Factory para retornar a Classe Calculadora de Imposto de acordo com a quantidade de meses informado para o investimento, de acordo com as regras definidas no escopo.

--- Nivelamento_GTF_202408.CalculoCDB/CalculoCDB.Tests/CalculoCDB/Servico/CalculoCDB_TestesServico.cs
+++ Nivelamento_GTF_202408.CalculoCDB/CalculoCDB.Tests/CalculoCDB/Servico/CalculoCDB_TestesServico.cs
@@ -1,12 +1,18 @@
-﻿namespace CalculoCDB.Tests.CalculoCDB.Servico
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CalculoCDB.Tests.CalculoCDB.Servico
 {
     public class CalculoCdb_TestesServico
     {
         private readonly ICalculoCdbService _calculoCDBService;
 
+        private readonly IImpostoFactory _impostoFactory;
+
         public CalculoCdb_TestesServico()
         {
-            _calculoCDBService = new CalculoCdbService();
+            _impostoFactory = new ImpostoFactory();
+            _calculoCDBService = new CalculoCdbService(_impostoFactory);
         }
 
         [Fact(DisplayName = "Deve Efetuar um Calculo do CBD com sucesso")]
@@ -15,7 +21,7 @@
             //Arrange
 
             //Act
-            var resultadoCalculadoInvestimento = _calculoCDBService.CalcularInvestimentoCBD(4, 1200m);
+            var resultadoCalculadoInvestimento = _calculoCDBService.CalcularInvestimentoCBD(7, 1200m);
 
             //Assert
             Assert.True(
@@ -28,6 +34,13 @@
 
     public class CalculoCdbService : ICalculoCdbService
     {
+        private readonly IImpostoFactory _impostoFactory;
+
+        public CalculoCdbService(IImpostoFactory impostoFactory)
+        {
+            _impostoFactory = impostoFactory;
+        }
+
         /// <summary>
         /// Efetua o Cáculo para um Investimento CBD
         /// </summary>
@@ -43,8 +56,10 @@
             for (int i = 0; i < prazo; i++)
             {
                 valorFinalBruto *= EfetuarCalculoCBD();
-                valorFinalLiquido += valorFinalBruto;
             }
+
+            ICalculadorImposto calculadorImposto = _impostoFactory.GetCalculadorImposto(prazo);
+            valorFinalLiquido = calculadorImposto.CalcularImposto(valorFinalBruto);
 
             var investimentoCalculado = new InvestimentoCalculado(valorFinalBruto, valorFinalLiquido);
 
@@ -53,15 +68,46 @@
 
         static decimal EfetuarCalculoCBD()
         {
-            decimal porcentagemCDI = 0.9m;
-            decimal procentagemTB = 108m;
+            decimal taxaCDI = 0.9m;
+            decimal taxaTB = 108m;
 
-            return 1 + (PercentualTaxa(porcentagemCDI) * PercentualTaxa(procentagemTB));
+            return 1 + (taxaCDI.PercentualTaxa() * taxaTB.PercentualTaxa());
+        }
+    }
+
+    public static class DecimalExtension
+    {
+        /// <summary>
+        /// Retorna a Porcentagem de uma Taxa
+        /// </summary>
+        /// <param name="taxa"></param>
+        /// <returns></returns>
+        public static decimal PercentualTaxa(this decimal taxa)
+        {
+            if (taxa == 0)
+                throw new ArgumentException("Valor da Taxa não pode ser igual a Zero!");
+
+            return taxa / 100;
         }
 
-        static decimal PercentualTaxa(decimal taxa)
+        /// <summary>
+        /// Cálcula o Imposto sobre um Valor Bruto e retorna o Valor Liquído
+        /// </summary>
+        /// <param name="valorBruto"></param>
+        /// <param name="imposto"></param>
+        /// <returns></returns>
+        public static decimal CalcularValorComImposto(this decimal valorBruto, decimal imposto)
         {
-            return taxa / 100;
+            if (valorBruto == 0)
+                throw new ArgumentException("Valor Bruto não pode ser igual a Zero!");
+
+            if (imposto == 0)
+                throw new ArgumentException("Valor do Imposto não pode ser igual a Zero!");
+
+            decimal valorDesconto = valorBruto * imposto.PercentualTaxa();
+            decimal valorLiquido = valorBruto - valorDesconto;
+
+            return valorLiquido;
         }
     }
 
@@ -75,6 +121,7 @@
         /// <returns>ValorFinalBruto, ValorFinalLiquido</returns>
         Resultado<InvestimentoCalculado> CalcularInvestimentoCBD(int prazo, decimal valorInvestir);
     }
+
 
     public class Resultado<T>
     {
@@ -102,6 +149,136 @@
         {
             ValorFinalBruto = valorFinalBruto;
             ValorFinalLiquido = valorFinalLiquido;
+        }
+    }
+
+    public interface IImpostoFactory
+    {
+        ICalculadorImposto GetCalculadorImposto(int quantidadeMeses);
+    }
+
+    public class ImpostoFactory : IImpostoFactory
+    {
+        /// <summary>
+        /// Retorna um Calculador de Imposto de acordo com a Quantidade de Meses Informada
+        /// </summary>
+        /// <param name="quantidadeMeses"></param>
+        /// <returns></returns>
+        public ICalculadorImposto GetCalculadorImposto(int quantidadeMeses)
+        {
+            if (quantidadeMeses == 0)
+                throw new ArgumentException("Quantidade de Meses não pode ser igual a Zero!");
+
+            return quantidadeMeses switch
+            {
+                var _ when quantidadeMeses <= 6 => new ImpostoAteSeisMeses(),
+                var _ when quantidadeMeses is > 6 and <= 12 => new ImpostoAteDozeMeses(),
+                var _ when quantidadeMeses is > 12 and <= 24 => new ImpostoAteVinteQuatroMeses(),
+                var _ when quantidadeMeses > 24 => new ImpostoAcimaDeVinteQuatroMeses(),
+                _ => throw new ArgumentException("Imposto não configurado para a Quantidade de Meses informada."),
+            };
+        }
+    }
+
+    public interface ICalculadorImposto
+    {
+        /// <summary>
+        /// Assinatura de Método para Calcular Diferentes Tipos De Impostos
+        /// </summary>
+        /// <param name="valorBruto"></param>
+        /// <returns></returns>
+        decimal CalcularImposto(decimal valorBruto);
+    }
+
+    public class ImpostoAteSeisMeses : ICalculadorImposto
+    {
+        private readonly decimal _imposto;
+
+        public ImpostoAteSeisMeses()
+        {
+            // Definido no Construtor para fins de futuras implemenções na definição do valor no AppSettings ou XML
+            // Imposto Até 06 meses: 22,5%
+            _imposto = 22.5m;
+        }
+
+        /// <summary>
+        /// Cáclula o Imposto para investimentos de até 06 meses
+        /// </summary>
+        /// <param name="valorBruto"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public decimal CalcularImposto(decimal valorBruto)
+        {
+            decimal valorDesconto = valorBruto * _imposto.PercentualTaxa();
+            decimal valorLiquido = valorBruto - valorDesconto;
+
+            return valorLiquido;
+        }
+    }
+
+    public class ImpostoAteDozeMeses : ICalculadorImposto
+    {
+        private readonly decimal _imposto;
+
+        public ImpostoAteDozeMeses()
+        {
+            // Definido no Construtor para fins de futuras implemenções na definição do valor no AppSettings ou XML
+            // Imposto Até 06 meses: 22,5%
+            _imposto = 22.5m;
+        }
+
+        /// <summary>
+        ///  Cáclula o Imposto para investimentos de até 12 meses
+        /// </summary>
+        /// <param name="valorBruto"></param>
+        /// <returns></returns>
+        public decimal CalcularImposto(decimal valorBruto)
+        {
+            return valorBruto.CalcularValorComImposto(_imposto);
+        }
+    }
+
+    public class ImpostoAteVinteQuatroMeses : ICalculadorImposto
+    {
+        private readonly decimal _imposto;
+
+        public ImpostoAteVinteQuatroMeses()
+        {
+            // Definido no Construtor para fins de futuras implemenções na definição do valor no AppSettings ou XML
+            // Imposto Até 06 meses: 22,5%
+            _imposto = 22.5m;
+        }
+
+        /// <summary>
+        ///  Cáclula o Imposto para investimentos de até 24 meses
+        /// </summary>
+        /// <param name="valorBruto"></param>
+        /// <returns></returns>
+        public decimal CalcularImposto(decimal valorBruto)
+        {
+            return valorBruto.CalcularValorComImposto(_imposto);
+        }
+    }
+
+    public class ImpostoAcimaDeVinteQuatroMeses : ICalculadorImposto
+    {
+        private readonly decimal _imposto;
+
+        public ImpostoAcimaDeVinteQuatroMeses()
+        {
+            // Definido no Construtor para fins de futuras implemenções na definição do valor no AppSettings ou XML
+            // Imposto Até 06 meses: 22,5%
+            _imposto = 22.5m;
+        }
+
+        /// <summary>
+        ///  Cáclula o Imposto para investimentos acima de 24 meses
+        /// </summary>
+        /// <param name="valorBruto"></param>
+        /// <returns></returns>
+        public decimal CalcularImposto(decimal valorBruto)
+        {
+            return valorBruto.CalcularValorComImposto(_imposto);
         }
     }
 }
