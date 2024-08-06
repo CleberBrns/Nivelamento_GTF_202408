17716b23ac888f2dab22f27617e98922f9df06a5
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2024-08-06 18:46:14 -0300

    Inclusão do projeto Angular
    Inclusão do projeto Angular ainda sem as funcionalidades de comunicação com a API e a tela para Calcular o Imposto

---
+++ Nivelamento_GTF_202408.CalculoInvestimentos/CalculoInvestimentos.WebUI/calculo-investimentos/src/app/modules/calculador-investimentos/calculo-cdb/calculo-cdb.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculo-cdb',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculo-cdb.component.html',
+  styleUrl: './calculo-cdb.component.css'
+})
+export class CalculoCdbComponent {
+
+}
