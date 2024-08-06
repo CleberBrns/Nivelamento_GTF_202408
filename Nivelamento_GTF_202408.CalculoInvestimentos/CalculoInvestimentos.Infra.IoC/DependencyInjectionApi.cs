229128c23bb16de515c92cbb3d1ebf8feac7863d
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2024-08-06 09:51:35 -0300

    Criação do Projeto de Inversão de Controle para injeção de dependência

---
+++ Nivelamento_GTF_202408.CalculoInvestimentos/CalculoInvestimentos.Infra.IoC/DependencyInjectionApi.cs
@@ -0,0 +1,19 @@
+﻿using Investimentos.Service.Interfaces.CalculoCdb;
+using Investimentos.Service.Interfaces.Imposto;
+using Investimentos.Service.Services.CalculoCdb;
+using Investimentos.Service.Services.Imposto;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CalculoInvestimentos.Infra.IoC
+{
+    public static class DependencyInjectionApi
+    {
+        public static IServiceCollection AddInfrastructureApi(this IServiceCollection services)
+        {
+            services.AddScoped<ICalculoCdbService, CalculoCdbService>();
+            services.AddScoped<IImpostoFactory, ImpostoFactory>();
+
+            return services;
+        }
+    }
+}
